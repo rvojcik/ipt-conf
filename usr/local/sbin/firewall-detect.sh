4a82f0d8c4e99756bce6c92626fe1f58759457ac
Jan Guttek <jan.guttek@tekian.eu>
2011-09-27 20:52:08 +0000

    adding usr/local/sbin/firewall-detect.sh

---
+++ usr/local/sbin/firewall-detect.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+if (( $# < 1 )); then
+	echo -e "Usage: $0 [search..]"
+	exit 2
+fi
+
+function netstat_filter {
+	netstat -tulpn 2> /dev/null | awk '/'$1'/ {
+		PROTOCOL=$1
+
+		split($7,ARRAY,"/")
+		PROCESS=ARRAY[2]
+
+		split($4,ARRAY,":")
+		INTERFACE=ARRAY[1]
+		PORT=ARRAY[2]
+
+		print PROCESS" \t"INTERFACE" \t"PROTOCOL" \t\t"PORT
+	}'
+}
+
+echo -e "PROCESS \tINTERFACE \tPROTOCOL \tPORT"
+while [[ "$1" != "" ]]; do
+	netstat_filter $1; shift
+done
